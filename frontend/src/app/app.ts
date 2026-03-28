a7cf6f54ff1c7900e459b773811c46e3071b4c15
Mika-Kuh <160069667+Mika-Kuh@users.noreply.github.com>
2026-03-28 22:06:23 +0100

    corrected styling for creation-page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -25,6 +25,7 @@ export class App {
       isActive("/register", this.router, matchOptions)()
       || isActive("/login", this.router, matchOptions)()
       || isActive("/change-profile", this.router, matchOptions)()
+      || isActive("/create-recipe", this.router, matchOptions)()
     );
   }
 
